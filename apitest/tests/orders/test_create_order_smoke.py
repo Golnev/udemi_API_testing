52dc57b92a9cc9efbda43fcfcdfbb9f1089bb86f
golnev <eugenegolnev1993@gmail.com>
2025-01-27 21:06:25 +0100

    add setup fixture fo test cases id=48 and id=49

--- apitest/tests/orders/test_create_order_smoke.py
+++ apitest/tests/orders/test_create_order_smoke.py
@@ -5,19 +5,30 @@ from apitest.src.helpers.customers_helper import CustomerHelper
 from apitest.src.helpers.orders_helper import OrdersHelper
 
 
+@pytest.fixture(scope="module")
+def my_orders_smoke_setup():
+    product_dao = ProductsDAO()
+
+    rand_product = product_dao.get_random_product_from_db(1)
+    product_id = rand_product[0]['ID']
+
+    order_helper = OrdersHelper()
+
+    info = {'product_id': product_id, 'order_helper': order_helper}
+
+    return info
+
+
 @pytest.mark.smoke
 @pytest.mark.orders
 @pytest.mark.tcid48
-def test_create_paid_order_guest_user():
+def test_create_paid_order_guest_user(my_orders_smoke_setup):
     #  Create helper objects.
-    product_dao = ProductsDAO()
-    order_helper = OrdersHelper()
+    order_helper = my_orders_smoke_setup['order_helper']
 
     customer_id = 0
+    product_id = my_orders_smoke_setup['product_id']
 
-    #  Get a product from DB.
-    rand_product = product_dao.get_random_product_from_db(1)
-    product_id = rand_product[0]['ID']
 
     #  Make the call.
     info = {"line_items": [
@@ -36,19 +47,15 @@ def test_create_paid_order_guest_user():
 @pytest.mark.smoke
 @pytest.mark.orders
 @pytest.mark.tcid49
-def test_create_paid_order_new_created_customer():
+def test_create_paid_order_new_created_customer(my_orders_smoke_setup):
     #  Create helper objects.
-    product_dao = ProductsDAO()
-    order_helper = OrdersHelper()
+    order_helper = my_orders_smoke_setup['order_helper']
     customer_helper = CustomerHelper()
-
-    #  Get a product from DB.
-    rand_product = product_dao.get_random_product_from_db(1)
-    product_id = rand_product[0]['ID']
 
     #  Make the call.
     cust_info = customer_helper.create_customer()
     customer_id = cust_info['id']
+    product_id = my_orders_smoke_setup['product_id']
 
     info = {"line_items": [
         {
