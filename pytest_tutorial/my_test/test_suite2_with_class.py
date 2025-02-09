a1279b326520caabc8a556240b0215ebf3ff599f
golnev <eugenegolnev1993@gmail.com>
2025-02-09 20:10:11 +0100

    refoctoring with black in nvim

--- pytest_tutorial/my_test/test_suite2_with_class.py
+++ pytest_tutorial/my_test/test_suite2_with_class.py
@@ -3,23 +3,23 @@ import pytest
 pytestmark = [pytest.mark.fe, pytest.mark.slow]
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def my_setup():
-    print('')
-    print('>>> MY SETUP <<<')
+    print("")
+    print(">>> MY SETUP <<<")
 
-    return {'id': 20, 'name': 'Admas'}
+    return {"id": 20, "name": "Admas"}
 
 
 @pytest.mark.abc
 class TestCheckout:
 
     def test_checkout_as_guest(self, my_setup):
-        print('\n')
-        print('Checkout as guest')
-        print('Class method: 1')
+        print("\n")
+        print("Checkout as guest")
+        print("Class method: 1")
 
     def test_checkout_with_existing_user(self):
-        print('\n')
-        print('Checkout as user')
-        print('Class method: 2')
+        print("\n")
+        print("Checkout as user")
+        print("Class method: 2")
